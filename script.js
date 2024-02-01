c22db7e24ca66207a94536835cfdd05b8fb4e23f
Leonardo Coutinho <leoctc@hotmail.com>
2024-01-31 22:34:04 -0300

    update commit

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ async function buscarEMostrarVideos() {
     ? "https://gist.githubusercontent.com/antonio-evaldo/e8a63621b51c883931eb3fa3a3eca990/raw/12f5c46ee6dd00d03c051adadaf341e06452cea0/videos.txt"
     : "http://localhost:3000/videos";
 
+  console.log(urlVideos);
+
   try {
     const busca = await axios.get(urlVideos);
 
